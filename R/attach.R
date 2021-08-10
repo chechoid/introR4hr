9aec134b0b875ca86949f0371f096cf896ff31b8
chechoid <sergio.garciamora@gmail.com>
2021-08-10 09:45:25 -0300

    Actualiza Description File y attach

--- R/attach.R
+++ R/attach.R
@@ -1,4 +1,4 @@
 .onAttach <- function(...)
 {
-  packageStartupMessage(sprintf("introR4hr v.%s :)\nTutoriales del Curso Introducción a R para RRHH\n /Data 4HR", packageVersion("introR4hr")))
+  packageStartupMessage(sprintf("introR4hr v.%s :)\nTutoriales del Curso Introducción a R para RRHH\nData 4HR\nwww.data-4hr.com", packageVersion("introR4hr")))
 }
